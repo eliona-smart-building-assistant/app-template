9672a56e27bac984e28424c991bfc87101560861
Andreas Göbel <andreas.goebel@eliona.io>
2022-12-14 07:25:49 +0100

    updated to new versions of dependencies

--- generate-api-server.sh
+++ generate-api-server.sh
@@ -2,7 +2,7 @@ go install golang.org/x/tools/cmd/goimports@latest
 
 docker run --rm \
     -v "${PWD}:/local" \
-    openapitools/openapi-generator-cli:v6.0.1 generate \
+    openapitools/openapi-generator-cli:v6.2.1 generate \
     -g go-server \
     --git-user-id eliona-smart-building-assistant \
     --git-repo-id python-eliona-api-client \
