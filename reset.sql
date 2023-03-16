d119c5a9bfa08d4dcdfd9fdc3397e05017a7a7f6
Zdenek Devaty <zdenek.devaty@eliona.io>
2023-03-16 14:33:56 +0100

    add db reset script

---
+++ reset.sql
@@ -0,0 +1,42 @@
+--  This file is part of the eliona project.
+--  Copyright © 2023 LEICOM iTEC AG. All Rights Reserved.
+--  ______ _ _
+-- |  ____| (_)
+-- | |__  | |_  ___  _ __   __ _
+-- |  __| | | |/ _ \| '_ \ / _` |
+-- | |____| | | (_) | | | | (_| |
+-- |______|_|_|\___/|_| |_|\__,_|
+--
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+--  BUT NOT LIMITED  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+--  NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+--  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-- This idempotent script resets the database to a defined state ready for testing.
+-- The only thing that remains after testing then are the incremented auto-increment values and app
+-- registration (which you can optionally remove as well by uncommenting the last command).
+
+INSERT INTO eliona_app (app_name, category, enable)
+VALUES ('template', 'app', 't')
+ON CONFLICT (app_name) DO UPDATE SET initialized_at = null;
+
+DROP SCHEMA IF EXISTS template CASCADE;
+
+DELETE FROM heap
+WHERE asset_id IN (
+	SELECT asset_id
+	FROM asset
+	WHERE asset_type LIKE 'template_%'
+);
+
+DELETE FROM attribute_schema
+WHERE asset_type LIKE 'template_%';
+
+DELETE FROM asset
+WHERE asset_type LIKE 'template_%'; 
+
+DELETE FROM asset_type
+WHERE asset_type LIKE 'template_%';
+
+-- DELETE FROM eliona_app WHERE app_name = 'template';
