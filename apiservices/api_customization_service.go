1c99f17c47f507712c34b57047d99d69bdf0e136
Andreas Göbel <andreas.goebel@eliona.io>
2023-04-13 13:29:59 +0200

    move dashboard template names to metadata.json

---
+++ apiservices/api_customization_service.go
@@ -0,0 +1,42 @@
+//  This file is part of the eliona project.
+//  Copyright © 2022 LEICOM iTEC AG. All Rights Reserved.
+//  ______ _ _
+// |  ____| (_)
+// | |__  | |_  ___  _ __   __ _
+// |  __| | | |/ _ \| '_ \ / _` |
+// | |____| | | (_) | | | | (_| |
+// |______|_|_|\___/|_| |_|\__,_|
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+//  BUT NOT LIMITED  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package apiservices
+
+import (
+	"context"
+	"net/http"
+	"template/apiserver"
+)
+
+// CustomizationApiService is a service that implements the logic for the CustomizationApiServicer
+// This service should implement the business logic for every endpoint for the CustomizationApi API.
+// Include any external packages or services that will be required by this service.
+type CustomizationApiService struct {
+}
+
+// NewCustomizationApiService creates a default api service
+func NewCustomizationApiService() apiserver.CustomizationApiServicer {
+	return &CustomizationApiService{}
+}
+
+// GetDashboardTemplateByName - Get a full dashboard template
+func (s *CustomizationApiService) GetDashboardTemplateByName(ctx context.Context, dashboardTemplateName string, projectId string) (apiserver.ImplResponse, error) {
+	if dashboardTemplateName == "Template" {
+		return apiserver.ImplResponse{Code: http.StatusNotImplemented}, nil
+	} else {
+		return apiserver.ImplResponse{Code: http.StatusNotFound}, nil
+	}
+}
