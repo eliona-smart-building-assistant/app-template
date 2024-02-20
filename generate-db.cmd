d3aab8afeb174bb1d02e5570087e715f13654abc
Andreas Göbel <andreas.goebel@eliona.io>
2024-02-20 14:01:12 +0100

    use integrated database for generation

--- generate-db.cmd
+++ generate-db.cmd
@@ -4,6 +4,20 @@ go install github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-psql@latest
 go get github.com/volatiletech/sqlboiler/v4
 go get github.com/volatiletech/null/v8
 
+docker run --rm -d ^
+    --name "app_sql_boiler_code_generation" ^
+    -e "POSTGRES_PASSWORD=secret" ^
+    -p "60001:5432" ^
+    -v "%cd%"\conf\init.sql:/docker-entrypoint-initdb.d/init.sql ^
+    debezium/postgres:12
+
+timeout /t 5
+
 sqlboiler psql ^
     -c sqlboiler.toml ^
     --wipe --no-tests
+
+docker stop "app_sql_boiler_code_generation"
+
+go mod tidy
+
--- generate-db.sh
+++ generate-db.sh
@@ -4,8 +4,19 @@ go install github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-psql@latest
 go get github.com/volatiletech/sqlboiler/v4
 go get github.com/volatiletech/null/v8
 
+docker run --rm -d \
+    --name "app_sql_boiler_code_generation" \
+    -e "POSTGRES_PASSWORD=secret" \
+    -p "60001:5432" \
+    -v "${PWD}/conf/init.sql:/docker-entrypoint-initdb.d/init.sql" \
+    debezium/postgres:12
+
+sleep 5
+
 sqlboiler psql \
     -c sqlboiler.toml \
     --wipe --no-tests
+
+docker stop "app_sql_boiler_code_generation"
 
 go mod tidy
