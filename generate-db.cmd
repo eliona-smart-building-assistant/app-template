34253a9ca7e78a8a3f1e13242b728d7724affa81
Zdenek Devaty <zdenek.devaty@eliona.io>
2024-02-21 08:48:30 +0100

    Improve DB generation
    To report errors and omit redundant information.

--- generate-db.cmd
+++ generate-db.cmd
@@ -4,12 +4,12 @@ go install github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-psql@latest
 go get github.com/volatiletech/sqlboiler/v4
 go get github.com/volatiletech/null/v8
 
-docker run --rm -d ^
+docker run -d ^
     --name "app_sql_boiler_code_generation" ^
     -e "POSTGRES_PASSWORD=secret" ^
     -p "60001:5432" ^
     -v "%cd%"\conf\init.sql:/docker-entrypoint-initdb.d/init.sql ^
-    debezium/postgres:12
+    debezium/postgres:12  > NUL
 
 timeout /t 5
 
@@ -17,7 +17,12 @@ sqlboiler psql ^
     -c sqlboiler.toml ^
     --wipe --no-tests
 
-docker stop "app_sql_boiler_code_generation"
+docker stop "app_sql_boiler_code_generation" > NUL
+
+docker logs "app_sql_boiler_code_generation" 2>&1 | findstr "ERROR" || (
+    echo All good.
+)
+
+docker rm "app_sql_boiler_code_generation" > NUL
 
 go mod tidy
-
